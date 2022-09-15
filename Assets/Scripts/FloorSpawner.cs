985a9862c22c09d95742375f248a6f81d345bc5d
Brianlatt <47317248+Brianlatt@users.noreply.github.com>
2022-09-15 14:16:59 +0100

    Working Platform underneeth of the player now spawns COrrectly

---
+++ Assets/Scripts/FloorSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FloorSpawner : MonoBehaviour
+{
+    public GameObject prefab; // Change this to private and serialie later on !!
+    Quaternion rotation = Quaternion.Euler(0, 0, 0);
+    public float TimeToStartFromSpawnRate = 0f;
+    public float TimeToNextSpawn = 10f;
+
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(Spawn), TimeToStartFromSpawnRate, TimeToNextSpawn);// Needs a provided Method.
+    }
+
+    private void Spawn() // Spawns The Set Prefab Of Pipes And Box Collector
+    {
+        GameObject WorldElements = Instantiate(prefab, transform.position, rotation); // Cloning the gameobject Written and setting it with position identity "
+
+    }
+}
