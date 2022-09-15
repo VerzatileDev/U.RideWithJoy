0370afbb1abeb9ae9b86d431f78826c54e02b886
Brianlatt <47317248+Brianlatt@users.noreply.github.com>
2022-09-15 10:42:17 +0100

    Basic Jump Setup And basic Props with additionally MainMenu Scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float JumpHeight = 3f;
+    public static bool iskilled = false;
+    //[SerializeField] AudioSource JumpSound;
+    //[SerializeField] AudioSource ExplosionSound;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        jump();
+    }
+
+    private void jump()
+    {
+        if (Input.GetButtonDown("Jump") && iskilled == false)
+        {
+            rb.velocity = (Vector2.up * JumpHeight);  // Change later and add deltatime..
+            //if (!hopSound.isPlaying && iskilled == false && PauseMenu.GameIsPaused == false)
+            //{
+                //hopSound.Play(); // <-- Add Sound later
+            //}
+        }
+    }
+
+    //Crouching ??  If In air Don't let them crouch if in power up mode don't let them maybe
+
+    // if player collided with Obstacle Freeze Time and Kill them, force Back into the Pause Menu...
+
+
+
+}
