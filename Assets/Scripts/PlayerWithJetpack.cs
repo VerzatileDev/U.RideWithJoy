7cff3e1dfd9991d341c1a912b644e27fcac9bb03
Brianlatt <lattb@coventry.ac.uk>
2022-09-16 02:26:19 +0100

    upgraded player movement too tired to continue, but now works with mouse / keyboard spacebar..  other stuff to be added maybe

---
+++ Assets/Scripts/PlayerWithJetpack.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class PlayerWithJetpack : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float jetPower = 70f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        bool jetEnabled = Input.GetButton("Jump");
+        if (jetEnabled)
+        {
+            rb.AddForce(new Vector2(0, jetPower));
+        }
+    }
+}
