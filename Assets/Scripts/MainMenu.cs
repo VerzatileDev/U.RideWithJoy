e9fe1bc3a581fba3dcbd2cf255495f2bab770469
Brianlatt <lattb@coventry.ac.uk>
2022-11-18 00:14:15 +0000

    Patch #2
    -Removed Jakes Folder.
    -Removed Unused Scenes.
    +Re enabled Death Menu that went missing.
    
    +Main Menu is now available
    +Credits are Available in Main Menu
    -Settings added, but work in progress currently..
    
    +Fixed Picture Resolution and quality.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenuUI;
+    [SerializeField] private GameObject settingsMenuUI;
+    [SerializeField] private GameObject CreditsUI;
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void StartScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+        Time.timeScale = 1f;
+    }
+
+    public void loadSettings()
+    {
+        mainMenuUI.SetActive(false);
+        settingsMenuUI.SetActive(true);
+    }
+
+    public void Credits()
+    {
+        mainMenuUI.SetActive(false);
+        CreditsUI.SetActive(true);
+    }
+
+    public void Back()
+    {
+        mainMenuUI.SetActive(true);
+        settingsMenuUI.SetActive(false);
+        CreditsUI.SetActive(false);
+    }
+}
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,6 +8,7 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private GameObject pauseMenuUI;
     [SerializeField] private GameObject settingsMenuUI;
     [SerializeField] private GameObject DiedMenuUI;
+    [SerializeField] private GameObject InGameUI;
     protected internal bool isdead = false;
  
     void Update()
@@ -33,20 +34,24 @@ public class PauseMenu : MonoBehaviour
         if (InsideSettings == false)
         {
             pauseMenuUI.SetActive(false);
+            InGameUI.SetActive(true);
             Time.timeScale = 1f;
             GameIsPaused = false;
         }
     }
+
     void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+        InGameUI.SetActive(false);
     }
     public void loadSettings()
     {
         //Debug.Log("Loading Settings");
         pauseMenuUI.SetActive(false);
+        InGameUI.SetActive(false);
         settingsMenuUI.SetActive(true);
         InsideSettings = true;
 
@@ -67,6 +72,7 @@ public class PauseMenu : MonoBehaviour
     public void DeathPause()
     {
         DiedMenuUI.SetActive(true);
+        InGameUI.SetActive(false);
         Time.timeScale = 0f;
     }
 
@@ -74,6 +80,7 @@ public class PauseMenu : MonoBehaviour
     {
         DiedMenuUI.SetActive(false);
         PlayerWithJetpack.iskilled = false;
+        InGameUI.SetActive(true);
 
         UnityEngine.SceneManagement.SceneManager.LoadScene("Main"); // Reloads Scene When ReTry.
         Time.timeScale = 1f;
